57d3f6942ecf70bb7560ff697d0f0093e43c2490
humbeatbox <154941844+humbeatbox@users.noreply.github.com>
2023-12-30 23:44:22 -0500

    Reset

---
+++ practice/practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  practice
+//
+//  Created by Gary Chang on 2023-12-30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
