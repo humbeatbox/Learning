2ec54c2c95749b54c8696e452304d95d3aef2bbf
humbeatbox <154941844+humbeatbox@users.noreply.github.com>
2024-01-02 23:25:30 -0500

    Just upload

--- practice/practice/main.c
+++ practice/practice/main.c
@@ -10,5 +10,10 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    printf("Input the number!\n");
+
+    int i;
+    scanf("%n",&i);
+    printf("Number is d\n",i);
     return 0;
 }
