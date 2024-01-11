b39c053eb8324444fc0a9b3551df8fa27da04fd9
humbeatbox <154941844+humbeatbox@users.noreply.github.com>
2024-01-11 14:16:20 -0500

    Put something

--- practice/practice/main.c
+++ practice/practice/main.c
@@ -6,16 +6,44 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-    printf("Input the number!\n");
-
-    int i=56;
-    scanf("%d",&i);
-    printf("Number is %d\n",i);
     
+    
+    struct node{
+        int data;
+        struct node *next;
+    };
+    
+    struct node *head, *mid, *last;
+    head = malloc(sizeof(struct node));
+    mid = malloc(sizeof(struct node));
+    last = malloc(sizeof(struct node));
+    
+    head->data = 10;
+    mid->data = 20;
+    last->data = 30;
+    
+    head->next = mid;
+    mid->next = last;
+    last->next  = NULL;
+    
+    struct node *temp = head;
+    
+    
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp = temp->next;
+        
+    }
+    printf("\n") ;
+    
+    printf("End!!\n");
     
     return 88;
 }
