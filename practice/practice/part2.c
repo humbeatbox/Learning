de5c9800b8425042cd119947f78cfd27304577d0
humbeatbox <hchang67@seneca.ca>
2024-01-13 23:07:07 -0500

    Update
    creat file

---
+++ practice/practice/part2.c
@@ -0,0 +1,57 @@
+//
+//  part2.c
+//  practice
+//
+//  Created by Gary Chang on 2024-01-12.
+//
+
+#include "part2.h"
+//
+//  main.c
+//  practice
+//
+//  Created by Gary Chang on 2023-12-30.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    
+    struct node{
+        int data;
+        struct node *next;
+    };
+    
+    struct node *head, *mid, *last;
+    head = malloc(sizeof(struct node));
+    mid = malloc(sizeof(struct node));
+    last = malloc(sizeof(struct node));
+    
+    head->data = 20;
+    mid->data = 40;
+    last->data = 60;
+    
+    head->next = mid;
+    mid->next = last;
+    last->next  = NULL;
+    
+    struct node *temp = head;
+    
+    
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp = temp->next;
+        
+    }
+    printf("\n") ;
+    
+    printf("End!!\n");
+    
+    return 88;
+}
---
+++ practice/practice/part2.h
@@ -0,0 +1,13 @@
+//
+//  part2.h
+//  practice
+//
+//  Created by Gary Chang on 2024-01-12.
+//
+
+#ifndef part2_h
+#define part2_h
+
+#include <stdio.h>
+
+#endif /* part2_h */
