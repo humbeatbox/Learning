4f1d5e22d34bd10ebcd4bc1dcac39d7048a82387
humbeatbox <hchang67@seneca.ca>
2024-03-10 15:54:25 -0400

    update arrayP

--- practice/arrayP/main.c
+++ practice/arrayP/main.c
@@ -4,15 +4,11 @@
 int main() {
     srand(time(0));
     int counter[6] = {0};
-
     for(int i = 1; i<=6000; ++i){
         int dice = rand() % 6+1;
-        for (int j = 1; j <= 6 ; ++j) {
-            if (dice == j){
-                counter[j-1]++;
-            }
-        }
+        counter[dice-1]++;
     }
+
     for (int j = 0; j < 6; ++j) {
         printf("Times of Number %d is %d\n",j+1,counter[j]);
     }
